3c1602e7c70e86a4d168a3082d379815c0b0153a
lerrybe <lerry597@gmail.com>
2022-07-25 18:37:16 +0900

    chore: setting react-query + recoil

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { RecoilRoot } from 'recoil';
+
 import EntryRoute from './routes';
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <EntryRoute />
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <EntryRoute />
+      </QueryClientProvider>
+    </RecoilRoot>
   </React.StrictMode>
 );
