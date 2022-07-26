d339ded97cc328a722908879b1434a7cccb0ca53
lerrybe <lerry597@gmail.com>
2022-07-27 03:14:48 +0900

    feat: implement gnb-mobile dropdown component

--- src/components/gnb/gnb.styled.tsx
+++ src/components/gnb/gnb.styled.tsx
@@ -1,4 +1,30 @@
 import styled from '@emotion/styled';
+import { motion } from 'framer-motion';
+
+export const DesktopWrapper = styled.div`
+  @media (min-width: 1056px) {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+  @media (max-width: 1055px) {
+    display: none;
+  }
+`;
+
+export const MobileWrapper = styled.div`
+  @media (min-width: 1056px) {
+    display: none;
+  }
+  @media (max-width: 1055px) {
+    width: 100%;
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+`;
 
 interface IWrapperProps {
   shadow?: boolean;
@@ -10,10 +36,13 @@ export const Wrapper = styled.div<IWrapperProps>(
   height: 72px;
   display: flex;
   align-items: center;
-  justify-content: space-between;
   padding: 20px 40px;
 
   ${shadow && 'box-shadow: 0 4px 12px 0 rgba(0, 0, 0, 0.1)'};
+
+  @media (max-width: 1055px) {
+    padding: 24px;
+  }
 `
 );
 
@@ -27,6 +56,21 @@ export const MenuItemWrapper = styled.div`
   align-items: center;
 `;
 
+interface IProps {
+  active?: boolean;
+}
+
+export const MobileMenuItemWrapper = styled(motion.div)<IProps>`
+  flex-direction: column;
+  position: absolute;
+  top: calc(100% + 8px);
+  right: 5px;
+  background-color: #fff;
+  box-shadow: 0 4px 12px 0 rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
+  overflow: hidden;
+`;
+
 export const MenuItemText = styled.span`
   font-size: 18px;
   font-weight: 500;
@@ -34,8 +78,25 @@ export const MenuItemText = styled.span`
   margin-right: 32px;
   cursor: pointer;
   transition: all 0.2s ease-out;
+
   &:hover {
     color: #00c471;
+  }
+
+  @media (max-width: 1055px) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin: 14px 30px;
+    font-size: 16px;
+
+    &:first-child {
+      margin-top: 24px;
+    }
+    &:last-child {
+      margin-bottom: 24px;
+    }
   }
 `;
 
@@ -45,4 +106,8 @@ export const ButtonWrapper = styled.div`
   &:last-child {
     margin-right: 0px;
   }
+`;
+
+export const MenuIconWrapper = styled.div`
+  cursor: pointer;
 `;
--- src/components/gnb/index.tsx
+++ src/components/gnb/index.tsx
@@ -1,9 +1,21 @@
+import { useCallback, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { ReactComponent as LogoLargeIcon } from '~/assets/icons/logo-large.svg';
+import { ReactComponent as LogoSmallIcon } from '~/assets/icons/logo-small.svg';
+import { ReactComponent as MenuIcon } from '~/assets/icons/icon-menu.svg';
 
 import { Button } from '../button';
-import { ButtonWrapper, LogoWrapper, MenuItemText, MenuItemWrapper, Wrapper } from './gnb.styled';
-import { ReactComponent as LogoLarge } from '~/assets/icons/logo-large.svg';
-import { useState } from 'react';
+import {
+  ButtonWrapper,
+  DesktopWrapper,
+  LogoWrapper,
+  MenuIconWrapper,
+  MenuItemText,
+  MenuItemWrapper,
+  MobileMenuItemWrapper,
+  MobileWrapper,
+  Wrapper,
+} from './gnb.styled';
 
 interface IGnbProps {
   shadow?: boolean;
@@ -11,27 +23,69 @@ interface IGnbProps {
 export const Gnb = ({ shadow }: IGnbProps) => {
   const navigate = useNavigate();
   const [token] = useState('sampleToken');
+  const [active, setActive] = useState(false);
+  const handleToggleMenu = useCallback(() => {
+    setActive((prev) => !prev);
+  }, [active]);
 
   return (
     <>
       <Wrapper shadow={shadow}>
-        <LogoWrapper onClick={() => navigate('/')}>
-          <LogoLarge />
-        </LogoWrapper>
-        <MenuItemWrapper>
-          <MenuItemText onClick={() => navigate('/')}>{'About'}</MenuItemText>
-          <MenuItemText onClick={() => navigate('/')}>{'Map'}</MenuItemText>
-          <MenuItemText onClick={() => navigate('/')}>{'Cards'}</MenuItemText>
-          {token && <MenuItemText onClick={() => navigate('/')}>{'Walkbook'}</MenuItemText>}
-          <ButtonWrapper>
-            <Button text={'산책로 등록하기'} />
-          </ButtonWrapper>
-          {token && (
-            <ButtonWrapper>
-              <Button text={'로그아웃'} />
-            </ButtonWrapper>
-          )}
-        </MenuItemWrapper>
+        <DesktopWrapper>
+          <LogoWrapper onClick={() => navigate('/')}>
+            <LogoLargeIcon />
+          </LogoWrapper>
+          <MenuItemWrapper>
+            <MenuItemText onClick={() => navigate('/')}>{'About'}</MenuItemText>
+            <MenuItemText onClick={() => navigate('/')}>{'Map'}</MenuItemText>
+            <MenuItemText onClick={() => navigate('/')}>{'Cards'}</MenuItemText>
+            {token ? (
+              <>
+                <MenuItemText onClick={() => navigate('/')}>{'Walkbook'}</MenuItemText>
+                <ButtonWrapper onClick={() => navigate('/')}>
+                  <Button text={'산책로 등록하기'} />
+                </ButtonWrapper>
+                <ButtonWrapper onClick={() => navigate('/')}>
+                  <Button text={'로그아웃'} />
+                </ButtonWrapper>
+              </>
+            ) : (
+              <ButtonWrapper onClick={() => navigate('/')}>
+                <Button text={'로그인 / 회원가입'} />
+              </ButtonWrapper>
+            )}
+          </MenuItemWrapper>
+        </DesktopWrapper>
+
+        <MobileWrapper>
+          <LogoWrapper>
+            <LogoSmallIcon />
+          </LogoWrapper>
+          <MenuIconWrapper>
+            <MenuIcon onClick={handleToggleMenu} />
+          </MenuIconWrapper>
+          <MobileMenuItemWrapper
+            initial={active ? 'open' : 'close'}
+            animate={active ? 'open' : 'close'}
+            variants={{ open: { height: 'auto' }, close: { height: 0 } }}
+            transition={{
+              type: 'ease-in-out',
+            }}
+          >
+            <MenuItemText onClick={() => navigate('/')}>{'About'}</MenuItemText>
+            <MenuItemText onClick={() => navigate('/')}>{'Map'}</MenuItemText>
+            <MenuItemText onClick={() => navigate('/')}>{'Cards'}</MenuItemText>
+            {token ? (
+              <>
+                <MenuItemText onClick={() => navigate('/')}>{'Walkbook'}</MenuItemText>
+                <MenuItemText onClick={() => navigate('/')}>{'산책로 등록하기'}</MenuItemText>
+                <MenuItemText onClick={() => navigate('/')}>{'로그아웃'}</MenuItemText>
+              </>
+            ) : (
+              <MenuItemText onClick={() => navigate('/')}>{'로그인 / 회원가입'}</MenuItemText>
+            )}
+          </MobileMenuItemWrapper>
+        </MobileWrapper>
       </Wrapper>
     </>
   );
