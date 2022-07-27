679082aa0f401d695e375b356ad4ff12b2292b76
lerrybe <lerry597@gmail.com>
2022-07-27 21:48:20 +0900

    feat: implement landing page

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,9 +1,52 @@
+import MarkerLargeImagePng from '~/assets/images/marker-large.png';
+import LandingBgLargeImagePng from '~/assets/images/landing-bg-large.png';
+
 import { Gnb } from '~/components/gnb';
+import { Button } from '~/components/button';
+
+import {
+  ButtonWrapper,
+  DescriptionWrapper,
+  LandingBgLargeImage,
+  LandingBgLargeImageWrapper,
+  MarkerLargeImage,
+  TextEmphasis,
+  TextNormal,
+  TextWrapper,
+  TitleWrapper,
+  Wrapper,
+} from './main.styled';
 
 const MainPage = () => {
   return (
     <>
       <Gnb />
+      <Wrapper>
+        <LandingBgLargeImageWrapper>
+          <LandingBgLargeImage src={LandingBgLargeImagePng} />
+          <MarkerLargeImage src={MarkerLargeImagePng} />
+        </LandingBgLargeImageWrapper>
+
+        <DescriptionWrapper>
+          <TextWrapper>
+            <TitleWrapper>
+              <TextNormal>{'함께 길을 읽다, '}</TextNormal>
+              <TextEmphasis>{'워크북'}</TextEmphasis>
+            </TitleWrapper>
+            <TextNormal>{'우리동네 산책로를 알아보고,'}</TextNormal>
+            <TextNormal>{'추천 산책로를 등록해보세요.'}</TextNormal>
+          </TextWrapper>
+          <ButtonWrapper>
+            <Button
+              width={'240px'}
+              height={'66px'}
+              radius={40}
+              text={'지금 시작하기'}
+              fontSize={20}
+            />
+          </ButtonWrapper>
+        </DescriptionWrapper>
+      </Wrapper>
     </>
   );
 };
