405637ddd0092e388ef27a8137e3cf107238ceda
lerrybe <lerry597@gmail.com>
2022-07-27 21:47:44 +0900

    feat: modify button component for custom

--- src/components/button/button.styled.tsx
+++ src/components/button/button.styled.tsx
@@ -6,22 +6,24 @@ interface WrapperProps extends HTMLAttributes<HTMLButtonElement> {
   height?: string;
   bgColor?: string;
   color?: string;
+  radius?: number;
+  fontSize?: number;
 }
 
 export const ButtonWrapper = styled.button<WrapperProps>(
-  ({ width, height, bgColor, color }) => `
+  ({ width, height, bgColor, color, radius, fontSize }) => `
   width: ${width};
   height: ${height};
   background-color: ${bgColor};
   color: ${color};
-  border-radius: 8px;
+  border-radius: ${`${radius}px`};
   padding: 12px 18px;
-  font-size: 16px;
+  font-size: ${`${fontSize}px`};
   font-weight: 600;
   transition: all 0.2s ease-out;
 
   &:hover {
-    background-color: #252525;
+    background-color: #00C471;
     color: #fff;
   }
 `
