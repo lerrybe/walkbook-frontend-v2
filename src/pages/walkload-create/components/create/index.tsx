4abb7f9620cbcca09e26eb68edd88d7e0a8919b0
lerrybe <lerry597@gmail.com>
2022-08-01 01:10:19 +0900

    feat: implement create component, refactor walkload-create component

---
+++ src/pages/walkload-create/components/create/index.tsx
@@ -0,0 +1,55 @@
+import InputLabel from '~/components/input-label';
+import { Wrapper, CreateTitle, TitleWrapper, EnrollTitle, ResetTitle } from './create.styled';
+
+const Create = () => {
+  return (
+    <Wrapper>
+      <TitleWrapper>
+        <ResetTitle>{'초기화'}</ResetTitle>
+        <CreateTitle>{'신규 산책로 추가'}</CreateTitle>
+        <EnrollTitle>{'등록하기'}</EnrollTitle>
+      </TitleWrapper>
+      <InputLabel
+        type={'text'}
+        labelText={'* 산책로 이름'}
+        placeholder={'산책로의 특징을 담은 이름을 입력해주세요.'}
+        onChange={() => console.log('test')}
+        bgColor={'#fff'}
+      />
+      <InputLabel
+        type={'text'}
+        labelText={'* 산책로 설명'}
+        placeholder={'해당 산책로의 특징을 소개해주세요.'}
+        onChange={() => console.log('test')}
+        bgColor={'#fff'}
+        margin={'16px'}
+      />
+      <InputLabel
+        type={'text'}
+        labelText={'* 시작위치'}
+        placeholder={'출발위치를 입력해주세요. ex) 낙성대역 1번출구'}
+        onChange={() => console.log('test')}
+        bgColor={'#fff'}
+        margin={'16px'}
+      />
+      <InputLabel
+        type={'text'}
+        labelText={'* 도착위치'}
+        placeholder={'도착위치를 입력해주세요. ex) 서울대학교 정문 앞'}
+        onChange={() => console.log('test')}
+        bgColor={'#fff'}
+        margin={'16px'}
+      />
+      <InputLabel
+        type={'text'}
+        labelText={'산책로 TMI'}
+        placeholder={'해당 산책로의 TMI를 알려주세요!'}
+        onChange={() => console.log('test')}
+        bgColor={'#fff'}
+        margin={'16px'}
+      />
+    </Wrapper>
+  );
+};
+
+export default Create;
