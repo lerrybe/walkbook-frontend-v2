5f3cccb8ed01e20497fb19caf77f5669df45a97f
lerrybe <lerry597@gmail.com>
2022-08-01 01:50:30 +0900

    feat: implement Error Page

--- src/pages/error/index.tsx
+++ src/pages/error/index.tsx
@@ -1,5 +1,52 @@
+import { useNavigate } from 'react-router-dom';
+
+import { Gnb } from '~/components/gnb';
+import { Button } from '~/components/button';
+
+import MarkerLargeImagePng from '~/assets/images/marker-large.png';
+import LandingBgLargeImagePng from '~/assets/images/landing-bg-large.png';
+
+import {
+  ButtonWrapper,
+  DescriptionWrapper,
+  LandingBgLargeImage,
+  LandingBgLargeImageWrapper,
+  MarkerLargeImage,
+  TextNormal,
+  TextWrapper,
+  Wrapper,
+} from './error.styled';
+
 const ErrorPage = () => {
-  return <div>잘못된 접근입니다.</div>;
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <Gnb />
+      <Wrapper>
+        <LandingBgLargeImageWrapper>
+          <LandingBgLargeImage src={LandingBgLargeImagePng} />
+          <MarkerLargeImage src={MarkerLargeImagePng} />
+        </LandingBgLargeImageWrapper>
+
+        <DescriptionWrapper>
+          <TextWrapper>
+            <TextNormal>{'😢 잘못된 접근입니다.'}</TextNormal>
+          </TextWrapper>
+          <ButtonWrapper>
+            <Button
+              width={'230px'}
+              height={'66px'}
+              radius={40}
+              text={'홈으로 돌아가기'}
+              fontSize={20}
+              onClick={() => navigate('/')}
+            />
+          </ButtonWrapper>
+        </DescriptionWrapper>
+      </Wrapper>
+    </>
+  );
 };
 
 export default ErrorPage;
