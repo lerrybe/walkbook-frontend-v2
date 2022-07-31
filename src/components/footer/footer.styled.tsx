3954ea6cfe4564ed6b7589dda76477d194a128d8
lerrybe <lerry597@gmail.com>
2022-07-31 20:03:36 +0900

    feat: implement Footer

---
+++ src/components/footer/footer.styled.tsx
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+export const Wrapper = styled.div`
+  width: 100%;
+  height: 84px;
+  background: #e8e8e8;
+  border-top: 0.8px solid rgba(0, 0, 0, 0.2);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #a4a4a4;
+  font-size: 15px;
+`;
